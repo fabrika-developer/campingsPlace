ef6a7b9857d32bca0cad293184dab09de035d0b1
davidsnege <david.snege@gmail.com>
2022-05-31 14:54:49 +0200

    ignore

--- slim/app/routes.php
+++ slim/app/routes.php
@@ -29,7 +29,7 @@ return function (App $app) {
     $app->get('/dbcase', function (Request $request, Response $response) {
 
         // Test SLIM Framework DB Connection
-                        
+        
 
     });
 
