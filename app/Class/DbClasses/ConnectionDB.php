daeb31eb3d110ed567c6357fd3025c63495e517c
davidsnege <david.snege@gmail.com>
2022-06-09 14:55:42 +0200

    update

--- app/Class/DbClasses/ConnectionDB.php
+++ app/Class/DbClasses/ConnectionDB.php
@@ -1,26 +1,62 @@
 <?php
 
-
-class ConnectionDB
-{
-    
-    public function getData($select, $table, $order)
+    class GeneralCrudSr
     {
+        // Connection to DB
+        public function connectionData()
+        {
+            $db = new PDO('mysql:host=10.5.0.5:3306;dbname=slimmysql', 'root', 'root');
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+            return $db;
+        }
+        // Select Data Query
+        public function selectAllDataFromTable($select,$from,$where,$order)
+        {
+            $db = $this->connectionData();
+            $sql = "SELECT ".$select." FROM ".$from." ".$where." ORDER BY ".$order."";
+            $stmt = $db->prepare($sql);
+            $stmt->execute();
+            $data = $stmt->fetchAll();
+            return $data;
+        }
+        // Insert data to table
+        public function insertDataToTable($database, $values)
+        {
+            $db = $this->connectionData();
+            $sql = "INSERT INTO ".$database." (".implode(",", array_keys($values)).") VALUES (:".implode(",:", array_keys($values)).")";
+            $stmt = $db->prepare($sql);
+            $stmt->execute($values);
+            $data = $stmt->fetchAll();
+            return $data;
+        }
+        // Update data to table
+        public function updateDataToTable($database, $values, $where)
+        {
+            $a = 0;
+            $setValue = '';
+            while ($a <= count($values)) {
+                $setValue .= "".array_keys($values)[$a]. '="' .array_values($values)[$a] . '", ';
+                $a++;
+            }
+            $aA = $a + 3;
+            $setValue2 = substr($setValue, 0, -$aA);
 
-        $db = new PDO('mysql:host=127.18.0.2:6033;dbname=slimmysql', 'root', 'root');
-
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        
-        $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-
-        // select all data from table
-        $sql = ' SELECT '. $select .' FROM '. $table . ' ORDER BY '. $order;
-        $stmt = $db->prepare($sql);
-        $stmt->execute();
-        $data = $stmt->fetchObject();
-
-        return $data;
-
+                $db = $this->connectionData();
+                $sql = "UPDATE ". $database ." 
+                SET ". $setValue2 ." 
+                WHERE ". $where ." ";
+                $stmt = $db->prepare($sql);
+                $stmt->execute();
+        }
+        // Delete data to table
+        public function deleteDataToTable($database, $where)
+        {
+            $db = $this->connectionData();
+            $sql = "DELETE FROM ".$database." WHERE ".$where."";
+            $stmt = $db->prepare($sql);
+            $stmt->execute();
+            $data = $stmt->fetchAll();
+            return $data;
+        }
     }
-
-}
--- app/Controllers/CheckDB.php
+++ app/Controllers/CheckDB.php
@@ -1,11 +1,41 @@
 <?php
-
 require_once('../Class/DbClasses/ConnectionDB.php');
 
-$newQuery = new ConnectionDB();
-$call = $newQuery->getData('*', 'test', 'id');
+$importClass = new GeneralCrudSr();
 
-echo $call->id;
-echo $call->name_user;
-echo $call->email;
+// Select Data Query
+    $query = $importClass->selectAllDataFromTable(' * ', ' test ', ' ', ' id ');
+    foreach ($query as $key => $value) {
+        echo '<pre>';
+        // echo $value->id;
+        print_r($value);
+        echo '</pre>';
+    };
+
+// Insert Data Query
+    // Prepare data to insert
+    $valuesInsert = array(
+        'name_user' => '555',
+        'email' => '555',
+        'tele' => '555'
+    );
+    // print_r($valuesInsert);
+    // call insertDataToTable
+    // $insert = $importClass->insertDataToTable('test', $valuesInsert);
+
+// Update Data Query
+    // Prepare data to update
+    $valuesUpdate = array(
+        'name_user' => 'qqqweqw',
+        'email' => 'sdfsfsdf',
+        'tele' => '2345345'
+    );
+    // call updateDataToTable
+    $update = $importClass->updateDataToTable('test', $valuesUpdate, ' id = 21 ');
+    // print_r($update);
+
+
+// Delete Data Query
+    // call deleteDataToTable
+    // $delete = $importClass->deleteDataToTable('test', ' id = 20 ');
 
