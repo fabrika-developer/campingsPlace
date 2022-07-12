f765cd8658198aae65e9da672ea2b2f74bc61bd6
davidsnege <david.snege@gmail.com>
2022-07-12 09:55:19 +0200

    Stage data

--- php/src/functions/load_all_campings_country.php
+++ php/src/functions/load_all_campings_country.php
@@ -4,29 +4,8 @@ require_once('classes/load_all_class.php');
 
 $load = new queryes_crud;
 
-echo '<div class="accordion accordion-flush" id="accordionFlushExample">
-<div class="accordion-item">
-  <h2 class="accordion-header" id="flush-headingOne">
-    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-      Accordion Item #1
-    </button>
-  </h2>
-  <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-    <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first items accordion body.</div>
-  </div>
-</div>
-</div>';
 
-echo '<script>
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 19,
-    attribution: "© OpenStreetMap"
-}).addTo(map);
-</script>';
-
-echo '<div id="map"></div>';
-
-$selectCampingByCountry = $load->selectAllDataFromTable('raisonSociale, addressCountry, addressRegion, addressDepartement, coordLatitude, coordLongitud', 'campings_general_data_use', '', 'id');
+$selectCampingByCountry = $load->selectAllDataFromTable('*', 'campings_general_data_use', '', 'id');
 foreach ($selectCampingByCountry as $key => $datoscp){
     echo '<pre>';
     $datoscp['raisonSociale'] = utf8_encode(strtoupper($datoscp['raisonSociale']));
--- php/src/functions/load_search_navbar.php
+++ php/src/functions/load_search_navbar.php
@@ -10,31 +10,59 @@ echo '<nav class="navbar sticky-top navbar-expand navbar-light bg-light" style="
   </button>
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+    &nbsp;
+
+
       <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="#">Home</a>
-      </li>
+
+
+        <input class="form-control form-inline" list="datalistOptions" id="exampleDataList" placeholder="Type to search...">
+        <datalist id="datalistOptions">
+          <option value="San Francisco">
+          <option value="New York">
+          <option value="Seattle">
+          <option value="Los Angeles">
+          <option value="Chicago">
+        </datalist>
+
+
+      </li>&nbsp;
       <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-          Dropdown
-        </a>
-        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <li><a class="dropdown-item" href="#">Action</a></li>
-          <li><a class="dropdown-item" href="#">Another action</a></li>
-          <li><hr class="dropdown-divider"></li>
-          <li><a class="dropdown-item" href="#">Something else here</a></li>
-        </ul>
-      </li>
+
+
+        <input class="form-control form-inline" list="datalistOptions" id="exampleDataList" placeholder="Type to search...">
+        <datalist id="datalistOptions">
+          <option value="San Francisco">
+          <option value="New York">
+          <option value="Seattle">
+          <option value="Los Angeles">
+          <option value="Chicago">
+        </datalist>
+
+
+      </li> &nbsp;
       <li class="nav-item">
-        <a class="nav-link disabled">Disabled</a>
+
+
+        <input class="form-control form-inline" list="datalistOptions" id="exampleDataList" placeholder="Type to search...">
+        <datalist id="datalistOptions">
+          <option value="San Francisco">
+          <option value="New York">
+          <option value="Seattle">
+          <option value="Los Angeles">
+          <option value="Chicago">
+        </datalist>
+
+
       </li>
+      &nbsp;
+      <li class="nav-item">
+      <button class="btn btn-inline-success" type="submit" style="background-color: #0a4d53; color: aliceblue;">Search</button>
+      </li>
+
     </ul>
-    <form class="d-flex">
-      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success" type="submit" style="background-color: #0a4d53;">Search</button>
-    </form>
+
+
   </div>
 </div>
 </nav>';
